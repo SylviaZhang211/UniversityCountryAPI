4afb7466e117c480f6b4088107f8dec1feee3075
SylviaZhang211 <sylviazhang211@gmail.com>
2024-06-05 18:30:47 -0700

    initialized

---
+++ src/main/java/com/example/universitycountryapi/UniversityCountryApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.universitycountryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCountryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityCountryApiApplication.class, args);
+    }
+
+}
